47bf7a564c22764ba17937acd83cfc57af9416d0
DorienPL <dorienpl@gmail.com>
2023-03-05 18:10:47 +0100

    Poprawka Listy

--- AppTest/ViewModels/ListView.cs
+++ AppTest/ViewModels/ListView.cs
@@ -1,6 +1,15 @@
+using System.Collections.Generic;
+
 namespace AppTest.ViewModels;
 
 public class ListView
 {
+    public List <string> Armia => new List<string>
+    { 
+        "Free Folk",
+        "Targaryen",
+        "Greyjoy",
+        "Night's Watch",
+    };
     
 }
--- AppTest/ViewModels/MainWindowViewModel.cs
+++ AppTest/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,12 @@
-﻿namespace AppTest.ViewModels;
+﻿using System.Collections.Generic;
+
+namespace AppTest.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia ha chuje!";
+    public string Greeting => "Witaj w aplikacji pomagającej tworzyć rozpiski do armii!";
+    
+    public string Testujemy => "Testowy napis";
+    private ListView _test => new ListView();
+    public List<string> test => _test.Armia;
 }
